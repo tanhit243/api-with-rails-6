0d416fde99a1d077ac5801decf43e267499a859a
anhvt-2280 <vu.tuan.anh-c@sun-asterisk.com>
2021-06-08 18:24:20 +0700

    Install config gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, default: { format: :json } do
     namespace :v1 do
-
+      devise_for :users,
+                 controllers: {
+                   sessions: 'users/sessions'
+                 },
+                 only: :sessions,
+                 path_names: { sign_in: :login, sign_out: :logout }
+      resources :users, only: :show
     end
   end
 end
